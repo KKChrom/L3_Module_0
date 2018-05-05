0ef1126bad01a13e85ddd65929d327ed25f7be1d
league <league@wts8.attlocal.net>
2018-05-05 14:31:42 -0700

    stack

--- src/IntroToStacks/IntroToStack.java
+++ src/IntroToStacks/IntroToStack.java
@@ -11,7 +11,7 @@ public class IntroToStack {
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
 		for(int i=0;i<100;i++)
 		{
-			double x = Math.random(100)+1;
+			double x = Math.random()*100;
 			rngdouble.push(x);
 		}
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
@@ -21,7 +21,17 @@ public class IntroToStack {
 		int num2 = Integer.parseInt(numb);
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
-		
+		System.out.println("Popping elements off stack... ");
+		System.out.println("Elements between "+num1+" and "+num2+" :");
+		for(int i=0;i<rngdouble.size();i++)
+		{
+			
+			double temp = rngdouble.pop();
+			if(temp>num1 && temp<num2)
+			{
+				System.out.println(temp);
+			}
+		}
 		//   EXAMPLE:
 		//   NUM 1: 65
 		//   NUM 2: 75
--- src/IntroToStacks/TextUndoRedo.java
+++ src/IntroToStacks/TextUndoRedo.java
@@ -1,9 +1,17 @@
 package IntroToStacks;
 
-public class TextUndoRedo {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TextUndoRedo implements KeyListener {
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
-	 * 
+	 
 	 * Every time a key is pressed, add that character to the JLabel. It should look like a basic text editor.
 	 * 
 	 * Make it so that every time the BACKSPACE key is pressed, the last character is erased from the JLabel.
@@ -13,5 +21,67 @@ public class TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
+	Stack <Character> s= new Stack<Character>();
+	Stack <Character> d= new Stack<Character>();        
+	JFrame Frame = new JFrame();
+	JPanel Panel = new JPanel();
+	JLabel Label = new JLabel("qqqqqqqqqqqqqqqqqq");
+	public static void main(String [] args) {
+		TextUndoRedo tur = new TextUndoRedo(); 
+		tur.create();
+	}
+	public String stacktostring()
+	{
+		String str = "";
+		for(int i=0;i<s.size();i++)
+		{
+			str += s.get(i);
+		}
+		return str;
+	}
+	public void create()
+	{
+		Frame.add(Panel);
+		Panel.add(Label);
+		Frame.setSize(500,500);
+		Frame.setVisible(true);
+		Panel.setVisible(true);
+		Frame.addKeyListener(this);
+		Frame.pack();
 	
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		char keycode = e.getKeyChar();
+		System.out.println(keycode);
+	
+			
+		if(keycode == KeyEvent.VK_BACK_SPACE)
+		{
+			d.push(s.pop());
+		}
+		else if(keycode==KeyEvent.VK_KP_UP)
+		{
+			s.push(d.pop());
+			System.out.println("mynamejeff");
+		}
+		else
+		{
+			s.push(keycode);
+		}
+		
+		Label.setText(stacktostring());
+		Frame.pack();
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
+
